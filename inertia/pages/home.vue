3496c5e944a8ad7b2a7a6c5b1d5a1f25768cad4a
Moumouuu <robin.travail007@gmail.com>
2024-08-16 19:02:01 +0200

    feat: add theme mode

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
-import { Head } from '@inertiajs/vue3'
-import { Button } from '~/components/ui/button'
 
 </script>
 
 <template>
-  <Head title="Homepage" />
 
-  <div class="flex flex-col items-center justify-center gap-4 py-12">
-
-    <Button>Hello</Button>
-
-  </div>
 </template>
