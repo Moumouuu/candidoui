3f41f84d51ea4d6bea5310d64c40c3b5eb1bcfe3
Moumouuu <robin.travail007@gmail.com>
2024-08-16 18:29:59 +0200

    feat: add tailwind css

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -7,12 +7,12 @@ import type { DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 
-const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
+const appName = import.meta.env.VITE_APP_NAME || "Candid'Oui"
 
 createInertiaApp({
   progress: { color: '#5468FF' },
 
-  title: (title) => `${title} - ${appName}`,
+  title: (title) => `${appName} - ${title}`,
 
   resolve: (name) => {
     return resolvePageComponent(
@@ -22,9 +22,9 @@ createInertiaApp({
   },
 
   setup({ el, App, props, plugin }) {
-    
+
     createSSRApp({ render: () => h(App, props) })
-    
+
       .use(plugin)
       .mount(el)
   },
